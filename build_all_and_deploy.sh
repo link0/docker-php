ba765460c1fcf5a33ef19bb109e9fd0856bc3450
Dennis de Greef <github@link0.net>
2018-04-11 22:29:35 +0200

    Add 7.2 and deprecate 5.6

--- build_all_and_deploy.sh
+++ build_all_and_deploy.sh
@@ -2,15 +2,14 @@
 
 set -e
 
-for version in 5.6 7.0 7.1;
+for version in 7.0 7.1 7.2;
 do
   ./build.sh $version;
   ./test.sh $version;
   ./deploy.sh $version;
 done
 
-docker tag link0/php:7.1 link0/php:latest;
-
+docker tag link0/php:7.2 link0/php:latest;
 docker push link0/php:latest;
 
 
